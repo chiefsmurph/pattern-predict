37d3a2abebc36cd472785ca15c8b526f8ef91028
chiefsmurph <chiefsmurph@gmail.com>
2021-01-24 13:41:18 -0600

    woops

--- scraping/basketballSportsbook.js
+++ scraping/basketballSportsbook.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 const getSportsbook = async () => {
 
-    const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
     await page.goto(`https://www.sportsline.com/nba/odds/`, {
         waitUntil: 'networkidle2',
